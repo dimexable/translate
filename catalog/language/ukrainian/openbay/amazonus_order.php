4801d1017e35fdd55a468d867c961bb372370804
dimexable <dimex2007@gmail.com>
2014-12-16 21:39:14 +0200

    Update amazonus_order.php

--- catalog/language/ukrainian/openbay/amazonus_order.php
+++ catalog/language/ukrainian/openbay/amazonus_order.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_paid_amazon'] 			= 'Paid on Amazon US';
-$_['text_total_shipping'] 		= 'Shipping';
+$_['text_total_shipping'] 		= 'Доставка';
 $_['text_total_shipping_tax'] 	= 'Shipping tax';
 $_['text_total_giftwrap'] 		= 'Gift wrap';
 $_['text_total_giftwrap_tax'] 	= 'Gift wrap tax';
